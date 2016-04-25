8a41bb5ad833993ee3db280b49cb57832b33da11
Danny Fritz <dannyfritz@gmail.com>
2016-04-25 16:17:18 -0600

    :sparkles: 6 Common Error exercises

---
+++ 03-SyntaxError/00.js
@@ -0,0 +1,6 @@
+// A Syntax Error
+// is when the JavaScript code is malformed and needs to be corrected
+// in order fo the JavaScript Lexer to understand it and run it.
+
+function getZero () {
+  return null;
---
+++ 03-SyntaxError/01.js
@@ -0,0 +1,7 @@
+// A Syntax Error
+// is when the JavaScript code is malformed and needs to be corrected
+// in order fo the JavaScript Lexer to understand it and run it.
+
+functin getOne () {
+  return "one"
+}
