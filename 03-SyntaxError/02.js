ae5f1c7fae8202de1a79d7f0df337f59d0a105d9
Danny Fritz <dannyfritz@gmail.com>
2016-04-25 16:20:16 -0600

    :books: Add link to MDN SyntaxError page

--- 03-SyntaxError/02.js
+++ 03-SyntaxError/02.js
@@ -1,4 +1,4 @@
-// A Syntax Error
+// A Syntax Error https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError
 // is when the JavaScript code is malformed and needs to be corrected
 // in order fo the JavaScript Lexer to understand it and run it.
 
